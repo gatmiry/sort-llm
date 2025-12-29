27c4e122c33c0a093125f1915cef56a4666b03cc
root <root@bolt-nwh8z2fbua-2sgzzndwtu.bolt-pods.turi-bolt.svc.cluster.local>
2025-12-28 21:49:55 -0800

    asdfads

--- next_num_statistics.py
+++ next_num_statistics.py
@@ -15,7 +15,7 @@ model_state_dict = torch.load(os.path.join(os.getcwd(), f'saved_models/dec28_tby
 model.load_state_dict(model_state_dict)
 model.to(device=device)
 
-def get_batch(batch_size=batch_size):
+def get_batch(batch_size=1):
    def cat_sorted_tensor(x):
       vals, _ = torch.sort(x)
       return torch.cat((x, torch.tensor([vocab_size]), vals), dim=0)
@@ -25,16 +25,29 @@ def get_batch(batch_size=batch_size):
 
 threshold = 0.05
 for i in range(instance_num):
-   idx = get_batch()
+   idx = get_batch(1)
+   print(f'idx shape is {idx.shape}')
    idx = idx.to(device=device)
    logits, loss = model(idx)
    all_candidates = []
    candidates = [[] for j in range(block_size, 2*block_size + 1)]
-   for j in range(block_size, 2*block_size + 1):
+   is_largest_score_correct = [None for j in range(block_size, 2*block_size + 1)]
+   for j in range(block_size, 2*block_size):
         for k in range(0, 2*block_size + 1):
-            if model.transformer.h[0].c_attn.attn[j,k] >= threshold:
-                candidates[j-block_size].append((k,idx[k]))
-                all_candidates.append((k,idx[k]))
-        print(f'candidates for position {j} are {candidates[j-block_size]}\n')    
+            max_score = float('-inf')
+            max_score_num = -1
+            score = model.transformer.h[0].c_attn.attn[j,k].item()
+            if score >= threshold:
+                if score > max_score:
+                    max_score = score
+                    max_score_num = idx[0,k].item()
+                candidates[j-block_size].append((k,idx[0,k].item()))
+                all_candidates.append((k,idx[0,k].item()))
+            #print('max_score_num is ', max_score_num, ' idx[0,j + 1].item() is ', idx[0,j + 1].item())
+            is_largest_score_correct[j-block_size] = max_score_num == idx[0,j + 1].item()    
 
+        print(f'candidates for position {j} with number {idx[0,j].item()} are {candidates[j-block_size]} \n \
+                is_largest_score_correct is {is_largest_score_correct[j-block_size]}\n')    
+        if is_largest_score_correct[j-block_size] == False:
+            print(f'scores for position {j} are {[(num, model.transformer.h[0].c_attn.attn[j,k].item()) for k, num in candidates[j-block_size]]}\n\n\n')
   

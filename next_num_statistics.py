54d5511f95c0f66138fe922eeba96dc4fbe764da
root <root@bolt-nwh8z2fbua-2sgzzndwtu.bolt-pods.turi-bolt.svc.cluster.local>
2025-12-28 23:54:34 -0800

    asdasdf

--- next_num_statistics.py
+++ next_num_statistics.py
@@ -1,6 +1,7 @@
 from model_tbyt_3 import GPT, GPTConfig
 import torch
 import os
+import numpy as np
 #block_size = 8
 #vocab_size = 128
 block_size = 32
@@ -24,30 +25,105 @@ def get_batch(batch_size=1):
    return x
 
 threshold = 0.05
-for i in range(instance_num):
-   idx = get_batch(1)
-   print(f'idx shape is {idx.shape}')
-   idx = idx.to(device=device)
-   logits, loss = model(idx)
-   all_candidates = []
-   candidates = [[] for j in range(block_size, 2*block_size + 1)]
-   is_largest_score_correct = [None for j in range(block_size, 2*block_size + 1)]
-   for j in range(block_size, 2*block_size):
-        for k in range(0, 2*block_size + 1):
+
+def get_statistics(thresholds, threshold_index):
+    clogit_cscore_perlocation = np.array([0.0 for i in range(block_size)])
+    clogit_icscore_perlocation = np.array([0.0 for i in range(block_size)])
+    iclogit_cscore_perlocation = np.array([0.0 for i in range(block_size)])
+    iclogit_icscore_perlocation = np.array([0.0 for i in range(block_size)])
+    clogit_cscore_perthreshold = []
+    clogit_icscore_perthreshold = []
+    iclogit_cscore_perthreshold = []
+    iclogit_icscore_perthreshold = []
+    
+    average_dist_perlocation = np.array([0.0 for i in range(block_size)])
+    max_dist_perlocation = np.array([0.0 for i in range(block_size)])
+    max_dist_perthreshold = []
+    average_dist_perthreshold = []
+    for t, threshold in enumerate(thresholds):
+        clogit_cscore = 0
+        clogit_icscore = 0
+        iclogit_cscore = 0
+        iclogit_icscore = 0
+        average_max_dist = 0.0
+        average_dist = 0.0
+        idx = get_batch(1)
+        print(f'idx shape is {idx.shape}')
+        idx = idx.to(device=device)
+        logits, loss = model(idx)
+        is_correct = torch.where(torch.argmax(logits[0 , block_size: 2 * block_size, :], dim=1) == idx[0, block_size + 1:], torch.ones(block_size).to(device), torch.zeros(block_size).to(device))
+        print(f'is_correct is {is_correct}')
+        all_candidates = []
+        
+        candidates = [[] for j in range(block_size, 2*block_size + 1)]
+        is_largest_score_correct = [None for j in range(block_size, 2*block_size + 1)]
+        for j in range(block_size, 2*block_size):
             max_score = float('-inf')
             max_score_num = -1
-            score = model.transformer.h[0].c_attn.attn[j,k].item()
-            if score >= threshold:
-                if score > max_score:
-                    max_score = score
-                    max_score_num = idx[0,k].item()
-                candidates[j-block_size].append((k,idx[0,k].item()))
-                all_candidates.append((k,idx[0,k].item()))
+            max_dist = 0.0
+            average_temp_dist = 0.0
+            dist = 0.0
+            num_dist = 0
+            for k in range(0, 2*block_size + 1):
+                score = model.transformer.h[0].c_attn.attn[j,k].item()
+                if score >= threshold:
+                    dist = abs(idx[0,k] - idx[0,j + 1])
+                    if dist > max_dist:
+                        max_dist = dist
+                    num_dist += 1
+                    average_temp_dist += dist
+                    if score > max_score:
+                        #print('im here')
+                        max_score = score
+                        max_score_num = idx[0,k].item()
+                        #print('max_score_num is ', max_score_num)
+                    candidates[j-block_size].append((k,idx[0,k].item()))
+                    all_candidates.append((k,idx[0,k].item()))
+            average_max_dist += max_dist
+            average_temp_dist /= num_dist
+            average_dist += average_temp_dist
             #print('max_score_num is ', max_score_num, ' idx[0,j + 1].item() is ', idx[0,j + 1].item())
-            is_largest_score_correct[j-block_size] = max_score_num == idx[0,j + 1].item()    
+            is_largest_score_correct[j-block_size] = (max_score_num == idx[0,j + 1].item())    
 
-        print(f'candidates for position {j} with number {idx[0,j].item()} are {candidates[j-block_size]} \n \
-                is_largest_score_correct is {is_largest_score_correct[j-block_size]}\n')    
-        if is_largest_score_correct[j-block_size] == False:
-            print(f'scores for position {j} are {[(num, model.transformer.h[0].c_attn.attn[j,k].item()) for k, num in candidates[j-block_size]]}\n\n\n')
-  
+            if t == threshold_index:
+                average_dist_perlocation[j-block_size] = average_temp_dist
+                max_dist_perlocation[j-block_size] = average_max_dist
+
+            if is_correct[j-block_size] == 1.0 and is_largest_score_correct[j-block_size] == True:
+                if t == threshold_index:
+                    clogit_cscore_perlocation[j-block_size] = 1.0
+                clogit_cscore += 1
+            elif is_correct[j-block_size] == 1.0 and is_largest_score_correct[j-block_size] == False:
+                if t == threshold_index:
+                    clogit_icscore_perlocation[j-block_size] = 1.0
+                clogit_icscore += 1
+            elif is_correct[j-block_size] == 0.0 and is_largest_score_correct[j-block_size] == True:
+                if t == threshold_index:
+                    iclogit_cscore_perlocation[j-block_size] = 1.0
+                iclogit_cscore += 1
+            elif is_correct[j-block_size] == 0.0 and is_largest_score_correct[j-block_size] == False:
+                if t == threshold_index:
+                    iclogit_icscore_perlocation[j-block_size] = 1.0
+                iclogit_icscore += 1
+
+            print(f'candidates for position {j} with number {idx[0,j].item()} and is_correct {is_correct[j-block_size] == 1.0} are {candidates[j-block_size]} \n \
+                    is_largest_score_correct is {is_largest_score_correct[j-block_size]}\n')    
+            if is_largest_score_correct[j-block_size] == False:
+                print(f'scores for position {j} are {[(num, model.transformer.h[0].c_attn.attn[j,k].item()) for k, num in candidates[j-block_size]]}\n\n\n')
+
+        average_max_dist /= block_size
+        average_dist /= block_size
+        max_dist_perthreshold.append(average_max_dist)
+        average_dist_perthreshold.append(average_dist)
+        ### added max_dist_perthreshold and average_dist_perthreshold to the list
+        clogit_cscore_perthreshold.append(clogit_cscore / block_size)
+        clogit_icscore_perthreshold.append(clogit_icscore / block_size)
+        clogit_cscore_perthreshold.append(iclogit_cscore / block_size)
+        iclogit_icscore_perthreshold.append(iclogit_icscore / block_size)
+    clogit_cscore_perlocation = [clogit_cscore_perlocation[i] / len(thresholds) for i in range(block_size)]
+    clogit_icscore_perlocation = [clogit_icscore_perlocation[i] / len(thresholds) for i in range(block_size)]
+    iclogit_cscore_perlocation = [iclogit_cscore_perlocation[i] / len(thresholds) for i in range(block_size)]
+    iclogit_icscore_perlocation = [iclogit_icscore_perlocation[i] / len(thresholds) for i in range(block_size)]
+    return (clogit_cscore_perlocation, clogit_icscore_perlocation, iclogit_cscore_perlocation, iclogit_icscore_perlocation), (clogit_cscore_perthreshold, clogit_icscore_perthreshold, iclogit_cscore_perthreshold, iclogit_icscore_perthreshold), (average_dist_perlocation, max_dist_perlocation), (average_dist_perthreshold, max_dist_perthreshold)
+
+
